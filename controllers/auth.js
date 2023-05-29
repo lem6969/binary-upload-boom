c08322bb72ce8793e7da7c2071d49aaf7476a243
lem <121071214+lem6969@users.noreply.github.com>
2023-05-29 10:30:24 +0800

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -116,3 +116,57 @@ exports.postSignup = (req, res, next) => {
     }
   );
 };
+
+
+//the code functionality
+
+/*This code exports several functions related to user authentication and account management using Passport.js, validator, and the User model.
+
+Here's an explanation of each function:
+
+    getLogin:
+        This function handles GET requests to the "/login" route.
+        If the user is already authenticated (req.user exists), it redirects to the "/profile" route.
+        Otherwise, it renders the "login" view, passing the title as "Login".
+
+    postLogin:
+        This function handles POST requests to the "/login" route.
+        It performs validation checks on the email and password inputs using the validator module.
+        If there are validation errors, they are flashed to the session and the user is redirected back to the "/login" route.
+        The email is normalized using validator.normalizeEmail() to ensure consistent formatting.
+        passport.authenticate("local", ...) is called to authenticate the user using the "local" strategy.
+        Inside the authentication callback, it checks for errors and if the user is not found, flashes an error message and redirects back to the "/login" route.
+        If the user is found and the password is correct, req.logIn() is called to establish a session and the user is redirected to the "/profile" route.
+        If there are any errors during the authentication process, the error is passed to the next middleware.
+
+    logout:
+        This function handles GET requests to the "/logout" route.
+        It calls req.logout() to remove the user's session.
+        req.session.destroy() is called to destroy the session, and any errors are logged to the console.
+        The user object is set to null (req.user = null).
+        Finally, it redirects the user to the "/" route.
+
+    getSignup:
+        This function handles GET requests to the "/signup" route.
+        If the user is already authenticated, it redirects to the "/profile" route.
+        Otherwise, it renders the "signup" view, passing the title as "Create Account".
+
+    postSignup:
+        This function handles POST requests to the "/signup" route.
+        It performs validation checks on the email, password, and confirmPassword inputs using the validator module.
+        If there are validation errors, they are flashed to the session and the user is redirected back to the "../signup" route.
+        The email is normalized using validator.normalizeEmail() to ensure consistent formatting.
+        A new User instance is created with the provided userName, email, and password.
+        It checks if the email or username already exists in the database using User.findOne().
+        If there is an existing user, an error message is flashed and the user is redirected back to the "../signup" route.
+        If there are no existing users, the new user is saved to the database and req.logIn() is called to establish a session.
+        Finally, the user is redirected to the "/profile" route.
+
+The code utilizes the Passport.js middleware for authentication and integrates with the User model to handle user login, logout, and signup functionality. It also uses the validator module for input validation and the flash module for storing flash messages in the session.*/
+
+
+
+
+
+
+
